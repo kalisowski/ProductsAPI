a532cb7304a9b2fc9a3bb4a644aa0cf5a08599bd
kalisowski <lisekgda3@gmail.com>
2023-01-12 18:08:55 +0100

    done

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use('/api/products', require('./routes/productRoutes'));
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
